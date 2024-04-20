dffe21fa941047e6236a998d3a61dd33af66766c
jshplayer <jshplayer@outlook.com>
2024-04-20 17:20:00 +1200

    Oce War Updates

---
+++ data/sql/db-world/1_prestigious_world_base.sql
@@ -0,0 +1,52 @@
+CREATE TABLE IF NOT EXISTS `prestige_rewards` (
+  `entry` int NOT NULL DEFAULT '0',
+  `count` int DEFAULT '1',
+  `scalable` tinyint DEFAULT NULL,
+  `prestige` int DEFAULT NULL,
+  `comment` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '',
+  PRIMARY KEY (`entry`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+DELETE FROM `prestige_rewards`;
+INSERT INTO `prestige_rewards` (`entry`, `count`, `scalable`, `prestige`, `comment`) VALUES
+	(37742, 5, 1, 0, 'Prestige Coin');
+
+CREATE TABLE IF NOT EXISTS `prestige_whitelist_quest` (
+  `quest_id` int NOT NULL,
+  `comment` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`quest_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+DELETE FROM `prestige_whitelist_quest`;
+INSERT INTO `prestige_whitelist_quest` (`quest_id`, `comment`) VALUES
+	(24545, 'Shadowmourne - 1 - The Sacred and the Corrupt'),
+	(24547, 'Shadowmourne - 3 - A Feast of Souls'),
+	(24548, 'Shadowmourne - 7 - The Splintered Throne'),
+	(24549, 'Shadowmourne - 9 - Shadowmourne...'),
+	(24743, 'Shadowmourne - 2 - Shadow\'s Edge'),
+	(24748, 'Shadowmourne - 10 - The Lich King\'s Last Stand'),
+	(24749, 'Shadowmourne - 4 - Unholy Infusion'),
+	(24756, 'Shadowmourne - 5 - Blood Infusion'),
+	(24757, 'Shadowmourne - 6 - Frost Infusion'),
+	(24912, 'Shadowmourne - 8 - Empowerment');
+
+CREATE TABLE IF NOT EXISTS `prestige_sacrifice_brackets` (
+  `itemlevel` int NOT NULL,
+  `multiplier` float DEFAULT NULL,
+  PRIMARY KEY (`itemlevel`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+DELETE FROM `prestige_sacrifice_brackets`;
+INSERT INTO `prestige_sacrifice_brackets` (`itemlevel`, `multiplier`) VALUES
+	(200, 1.5),
+	(216, 2.0),
+	(232, 3.0),
+	(245, 4.0),
+	(251, 5.0),
+	(255, 6.0),
+	(258, 7.0),
+	(262, 8.0),
+	(264, 9.0),
+	(266, 10.0),
+	(268, 11.0),
+	(270, 12.0);
--- data/sql/db-world/2_prestigious_world_npc.sql
+++ data/sql/db-world/2_prestigious_world_npc.sql
@@ -1,5 +1,6 @@
-DELETE FROM `creature_template` WHERE `entry`=555111;
-INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES (555111, 0, 0, 0, 0, 0, 10008, 0, 0, 0, 'Chromie', 'Timewalker', NULL, 0, 80, 80, 0, 35, 3, 1, 1.14286, 1, 1, 20, 1, 1, 0, 1, 0, 2000, 1, 1, 8, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1.23457, 1, 1, 1, 0, 0, 1, 0, 0, 0, 'PrestigiousNPCScript', 0);
+DELETE FROM `creature_template` WHERE (`entry` = 555111);
+INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
+(555111, 0, 0, 0, 0, 0, 860, 0, 0, 0, 'Oceanic War Prestiger', '', NULL, 0, 60, 60, 0, 35, 3, 1, 1.14286, 1, 1, 20, 1, 1, 0, 1, 0, 2000, 1, 1, 8, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1.23457, 1, 1, 1, 0, 0, 1, 0, 0, 0, 'PrestigiousNPCScript', 0);
 
 DELETE FROM `npc_text` WHERE `ID`=555123;
 INSERT INTO `npc_text` (`ID`, `text0_0`, `text0_1`, `BroadcastTextID0`, `lang0`, `Probability0`, `em0_0`, `em0_1`, `em0_2`, `em0_3`, `em0_4`, `em0_5`, `text1_0`, `text1_1`, `BroadcastTextID1`, `lang1`, `Probability1`, `em1_0`, `em1_1`, `em1_2`, `em1_3`, `em1_4`, `em1_5`, `text2_0`, `text2_1`, `BroadcastTextID2`, `lang2`, `Probability2`, `em2_0`, `em2_1`, `em2_2`, `em2_3`, `em2_4`, `em2_5`, `text3_0`, `text3_1`, `BroadcastTextID3`, `lang3`, `Probability3`, `em3_0`, `em3_1`, `em3_2`, `em3_3`, `em3_4`, `em3_5`, `text4_0`, `text4_1`, `BroadcastTextID4`, `lang4`, `Probability4`, `em4_0`, `em4_1`, `em4_2`, `em4_3`, `em4_4`, `em4_5`, `text5_0`, `text5_1`, `BroadcastTextID5`, `lang5`, `Probability5`, `em5_0`, `em5_1`, `em5_2`, `em5_3`, `em5_4`, `em5_5`, `text6_0`, `text6_1`, `BroadcastTextID6`, `lang6`, `Probability6`, `em6_0`, `em6_1`, `em6_2`, `em6_3`, `em6_4`, `em6_5`, `text7_0`, `text7_1`, `BroadcastTextID7`, `lang7`, `Probability7`, `em7_0`, `em7_1`, `em7_2`, `em7_3`, `em7_4`, `em7_5`, `VerifiedBuild`) VALUES (555123, 'You do not meet the requirements to prestige at this time.|n|nCome back to me when you have reached maximum level.', NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
@@ -9,54 +10,3 @@ INSERT INTO `npc_text` (`ID`, `text0_0`, `text0_1`, `BroadcastTextID0`, `lang0`,
 
 DELETE FROM `npc_text` WHERE `ID`=555125;
 INSERT INTO `npc_text` (`ID`, `text0_0`, `text0_1`, `BroadcastTextID0`, `lang0`, `Probability0`, `em0_0`, `em0_1`, `em0_2`, `em0_3`, `em0_4`, `em0_5`, `text1_0`, `text1_1`, `BroadcastTextID1`, `lang1`, `Probability1`, `em1_0`, `em1_1`, `em1_2`, `em1_3`, `em1_4`, `em1_5`, `text2_0`, `text2_1`, `BroadcastTextID2`, `lang2`, `Probability2`, `em2_0`, `em2_1`, `em2_2`, `em2_3`, `em2_4`, `em2_5`, `text3_0`, `text3_1`, `BroadcastTextID3`, `lang3`, `Probability3`, `em3_0`, `em3_1`, `em3_2`, `em3_3`, `em3_4`, `em3_5`, `text4_0`, `text4_1`, `BroadcastTextID4`, `lang4`, `Probability4`, `em4_0`, `em4_1`, `em4_2`, `em4_3`, `em4_4`, `em4_5`, `text5_0`, `text5_1`, `BroadcastTextID5`, `lang5`, `Probability5`, `em5_0`, `em5_1`, `em5_2`, `em5_3`, `em5_4`, `em5_5`, `text6_0`, `text6_1`, `BroadcastTextID6`, `lang6`, `Probability6`, `em6_0`, `em6_1`, `em6_2`, `em6_3`, `em6_4`, `em6_5`, `text7_0`, `text7_1`, `BroadcastTextID7`, `lang7`, `Probability7`, `em7_0`, `em7_1`, `em7_2`, `em7_3`, `em7_4`, `em7_5`, `VerifiedBuild`) VALUES (555125, 'If you would like greater rewards, you can sacrifice your equipped armor.|n|nThe higher the average item level the bigger the spoils, make your choice.', NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-
-CREATE TABLE IF NOT EXISTS `prestige_rewards` (
-  `entry` int NOT NULL DEFAULT '0',
-  `count` int DEFAULT '1',
-  `scalable` tinyint DEFAULT NULL,
-  `prestige` int DEFAULT NULL,
-  `comment` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '',
-  PRIMARY KEY (`entry`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-DELETE FROM `prestige_rewards`;
-INSERT INTO `prestige_rewards` (`entry`, `count`, `scalable`, `prestige`, `comment`) VALUES
-	(34493, 1, 0, 1, 'Dragon Kite - Count 1 - Don\'t scale with multipliers - Reward at prestige 1'),
-	(37742, 200, 1, 0, 'Currency Test Token 2 - Count 200 - Scale with multipliers - Reward for all prestiges');
-
-CREATE TABLE IF NOT EXISTS `prestige_whitelist_quest` (
-  `quest_id` int NOT NULL,
-  `comment` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  PRIMARY KEY (`quest_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-DELETE FROM `prestige_whitelist_quest`;
-INSERT INTO `prestige_whitelist_quest` (`quest_id`, `comment`) VALUES
-	(24545, 'Shadowmourne - 1 - The Sacred and the Corrupt'),
-	(24547, 'Shadowmourne - 3 - A Feast of Souls'),
-	(24548, 'Shadowmourne - 7 - The Splintered Throne'),
-	(24549, 'Shadowmourne - 9 - Shadowmourne...'),
-	(24743, 'Shadowmourne - 2 - Shadow\'s Edge'),
-	(24748, 'Shadowmourne - 10 - The Lich King\'s Last Stand'),
-	(24749, 'Shadowmourne - 4 - Unholy Infusion'),
-	(24756, 'Shadowmourne - 5 - Blood Infusion'),
-	(24757, 'Shadowmourne - 6 - Frost Infusion'),
-	(24912, 'Shadowmourne - 8 - Empowerment');
-
-CREATE TABLE IF NOT EXISTS `prestige_sacrifice_brackets` (
-  `itemlevel` int NOT NULL,
-  `multiplier` float DEFAULT NULL,
-  PRIMARY KEY (`itemlevel`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-DELETE FROM `prestige_sacrifice_brackets`;
-INSERT INTO `prestige_sacrifice_brackets` (`itemlevel`, `multiplier`) VALUES
-	(200, 1),
-	(216, 2),
-	(232, 3),
-	(245, 4),
-	(251, 5),
-	(264, 6),
-	(268, 7),
-	(270, 8);
-
